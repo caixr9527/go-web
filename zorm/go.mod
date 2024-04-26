15eefa6d9d3557e6e90013c1e70bbc14dffb2b65
caixiaorong <smart_r@163.com>
2024-04-26 23:47:38 +0800

    add feature 116

--- zorm/go.mod
+++ zorm/go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/go-playground/validator/v10 v10.19.0 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/nacos-group/nacos-sdk-go v1.1.4 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
---
+++ zorm/register/nacos.go
@@ -0,0 +1,55 @@
+package register
+
+import (
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/clients/naming_client"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
+)
+
+func CreateNacosClient() (naming_client.INamingClient, error) {
+	// todo 可以做成单例
+	clientConfig := *constant.NewClientConfig(
+		constant.WithNamespaceId(""),
+		constant.WithTimeoutMs(5000),
+		constant.WithNotLoadCacheAtStart(true),
+		constant.WithLogDir("/tmp/nacos/log"),
+		constant.WithCacheDir("/tmp/nacos/cache"),
+		constant.WithLogLevel("debug"),
+	)
+	serverConfigs := []constant.ServerConfig{
+		*constant.NewServerConfig(
+			"127.0.0.1",
+			8848,
+			constant.WithScheme("http"),
+			constant.WithContextPath("/nacos"),
+		),
+	}
+
+	client, err := clients.NewNamingClient(
+		vo.NacosClientParam{
+			ClientConfig:  &clientConfig,
+			ServerConfigs: serverConfigs,
+		})
+	if err != nil {
+		return nil, err
+	}
+	return client, err
+}
+
+// todo 可以优化，提取配置，自动获取ip
+func Register(client naming_client.INamingClient, serviceName string, ip string, port uint64) error {
+	_, err := client.RegisterInstance(vo.RegisterInstanceParam{
+		Ip:          ip,
+		Port:        port,
+		ServiceName: serviceName,
+		Weight:      10,
+		Enable:      true,
+		Healthy:     true,
+		Ephemeral:   true,
+		Metadata:    map[string]string{"idc": "shanghai"},
+		//ClusterName: "cluster-a",
+		//GroupName:   "group-a",
+	})
+	return err
+}
